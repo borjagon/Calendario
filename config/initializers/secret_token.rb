8ad5c817edf7c24bda3d96bad9bac371b053a656
borjagon <borjagon@gmail.com>
2013-05-08 12:12:29 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendario::Application.config.secret_token = '18bee57f0df9a67bcfef6afd4845a8deab4a2bfa801dc42c76c7334df83eadc7b7b806daca1f5157d464b14c23f4746e8652c4c90cbc579851962c814372f08f'
